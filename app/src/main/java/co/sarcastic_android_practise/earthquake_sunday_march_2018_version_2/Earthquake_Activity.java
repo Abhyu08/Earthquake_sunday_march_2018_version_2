581d1027cb20a30d4ec45c2f10ec2be57c0b55d9
Infinity <ghatale.abhyuday@gmail.com>
2018-03-18 12:54:37 +0530

    Initial commit

---
+++ app/src/main/java/co/sarcastic_android_practise/earthquake_sunday_march_2018_version_2/Earthquake_Activity.java
@@ -0,0 +1,13 @@
+package co.sarcastic_android_practise.earthquake_sunday_march_2018_version_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Earthquake_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_earthquake_);
+    }
+}
