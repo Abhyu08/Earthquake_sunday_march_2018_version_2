c4556cdd3e692425d326a568c45416b8699b11a8
Infinity <ghatale.abhyuday@gmail.com>
2018-03-18 14:17:38 +0530

    After Creating Layout.

--- app/src/main/java/co/sarcastic_android_practise/earthquake_sunday_march_2018_version_2/EathquakeAdapter.java
+++ app/src/main/java/co/sarcastic_android_practise/earthquake_sunday_march_2018_version_2/EathquakeAdapter.java
@@ -3,12 +3,11 @@ package co.sarcastic_android_practise.earthquake_sunday_march_2018_version_2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Earthquake_Activity extends AppCompatActivity {
+public class EathquakeAdapter extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_earthquake_);
+        setContentView(R.layout.activity_eathquake_adapter);
     }
-
 }
