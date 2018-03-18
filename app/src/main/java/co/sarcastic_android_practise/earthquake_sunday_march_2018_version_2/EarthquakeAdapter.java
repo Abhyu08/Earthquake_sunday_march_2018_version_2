3b57e9324e86303819c0cf942c4d376fac436a47
Infinity <ghatale.abhyuday@gmail.com>
2018-03-18 22:58:40 +0530

    Setting up.

--- app/src/main/java/co/sarcastic_android_practise/earthquake_sunday_march_2018_version_2/EarthquakeAdapter.java
+++ app/src/main/java/co/sarcastic_android_practise/earthquake_sunday_march_2018_version_2/EarthquakeAdapter.java
@@ -1,7 +1,7 @@
 package co.sarcastic_android_practise.earthquake_sunday_march_2018_version_2;
 
-
 import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -16,6 +16,7 @@ import java.util.ArrayList;
 public class EarthquakeAdapter extends ArrayAdapter<String> {
 
     private static String LOCATION_SEPERATOR = "of";
+
     EarthquakeAdapter(Context context, ArrayList<String> earthquakes) {
         super(context, 0, earthquakes);
     }
@@ -23,10 +24,10 @@ public class EarthquakeAdapter extends ArrayAdapter<String> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        View myView =  convertView;
-       // LayoutInflater inflater = inflater.inflate( getContext() , R.layout.activity_eathquake_adapter , true);
+        View myView = convertView;
+        // LayoutInflater inflater = inflater.inflate( getContext() , R.layout.activity_eathquake_adapter , true);
         if (myView == null) {
-            myView = LayoutInflater.from(getContext()).inflate( R.layout.activity_eathquake_adapter, parent, true);
+            myView = LayoutInflater.from(getContext()).inflate(R.layout.activity_eathquake_adapter, parent, true);
         }
 
         //Here Error can occur ------->
@@ -37,21 +38,20 @@ public class EarthquakeAdapter extends ArrayAdapter<String> {
         TextView locationView2 = (TextView) myView.findViewById(R.id.address2);
         String OriginalLocation = currentEarthquake.getPlace();
         String[] parts = OriginalLocation.split(LOCATION_SEPERATOR);
-        if( OriginalLocation.contains(LOCATION_SEPERATOR)){
+        if (OriginalLocation.contains(LOCATION_SEPERATOR)) {
             // Display the location of the current earthquake in that TextView
             locationView2.setText(parts[0]);
-            locationView.setText( " "+ parts[1]);
-        }
-        else{
+            locationView.setText(" " + parts[1]);
+        } else {
             // Display the location of the current earthquake in that TextView
             locationView.setText(parts[0]);
         }
 
         // Find the TextView with view ID magnitude
-        TextView magnitudeView =  myView.findViewById(R.id.magnitude);
+        TextView magnitudeView = myView.findViewById(R.id.magnitude);
         // Display the magnitude of the current earthquake in that TextView
         Double Magnitude = Double.parseDouble(currentEarthquake.getMagnitude());
-        magnitudeView.setText(""+Magnitude);
+        magnitudeView.setText("" + Magnitude);
 
         // Find the TextView with view ID date
         TextView dateView = (TextView) myView.findViewById(R.id.timetext);
@@ -60,7 +60,7 @@ public class EarthquakeAdapter extends ArrayAdapter<String> {
         // Display the date of the current earthquake in that TextView
         dateView.setText(formattedDate);
 
-       // String UrlFormat = currentEarthquake.getUrl();
+        // String UrlFormat = currentEarthquake.getUrl();
 
         // Find the TextView with view ID time
         TextView timeView = (TextView) myView.findViewById(R.id.time);
@@ -68,14 +68,61 @@ public class EarthquakeAdapter extends ArrayAdapter<String> {
         String formattedTime = formatTime(currentEarthquake.getTime());
         // Display the time of the current earthquake in that TextView
         timeView.setText(formattedTime);
-        
+
+        GradientDrawable MagnitudeDrawable = (GradientDrawable) magnitudeView.getBackground();
+        int MagnitudeColor = getMagnitudeColor(Magnitude);
+        MagnitudeDrawable.setColor(MagnitudeColor);
+
         return myView;
     }
-    public String formatDate(String date){
+
+    private int getMagnitudeColor(double MagnitudeColorId) {
+        int MagnitudeColorId2 = (int) MagnitudeColorId;
+        int MagnitudeColorId1;
+
+        switch (MagnitudeColorId2) {
+            case 1:
+                MagnitudeColorId1 = R.color.magnitude1;
+                break;
+            case 2:
+                MagnitudeColorId1 = R.color.magnitude2;
+                break;
+            case 3:
+                MagnitudeColorId1 = R.color.magnitude3;
+                break;
+            case 4:
+                MagnitudeColorId1 = R.color.magnitude4;
+                break;
+            case 5:
+                MagnitudeColorId1 = R.color.magnitude5;
+                break;
+            case 6:
+                MagnitudeColorId1 = R.color.magnitude6;
+                break;
+            case 7:
+                MagnitudeColorId1 = R.color.magnitude7;
+                break;
+            case 8:
+                MagnitudeColorId1 = R.color.magnitude8;
+                break;
+            case 9:
+                MagnitudeColorId1 = R.color.magnitude9;
+                break;
+            case 10:
+                MagnitudeColorId1 = R.color.magnitude10plus;
+                break;
+            default:
+                MagnitudeColorId1 = R.color.magnitude1;
+        }
+        return MagnitudeColorId1;
+    }
+
+    public String formatDate(String date) {
         SimpleDateFormat dateFormat = new SimpleDateFormat("LLL dd, yyyy");
         return dateFormat.format(date);
     }
-    public String formatTime(String time){
+
+    public String formatTime(String time) {
         SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a");
         return timeFormat.format(time);
     }
